e6de372af66d9b6be6ac04d30fdd90fc5eb2eac0
jackness <jackness1208@qq.com>
2018-03-21 22:17:07 +0800

    [EDIT] 用例新增完成

--- test/test.js
+++ test/test.js
@@ -220,4 +220,185 @@ describe('yyl-inlinesource test', () => {
       done();
     }).start();
   });
+
+
+
+  it('yyl-inlinesource({type: html}) abs test', function(done) {
+    this.timeout(0);
+    const srcPath = path.join(FRAG_PATH, '/html/demo.html');
+    const distPath = path.join(FRAG_PATH, 'r.html');
+    new util.Promise((next) => { // clear frag
+      fn.frag.destroy().then(() => {
+        fn.frag.build().then(() => {
+          next();
+        });
+      });
+    }).then((next) => { // copy files
+      const sourcePath = path.join(__dirname, 'dist');
+      util.copyFiles(sourcePath, FRAG_PATH, () => {
+        next();
+      });
+    }).then((next) => { // run tasks
+      inlinesource({
+        baseUrl: path.dirname(srcPath),
+        publishPath: '/html/',
+        type: 'html',
+        content: fs.readFileSync(srcPath)
+      }).then((cnt) => {
+        fs.writeFileSync(distPath, cnt);
+        next();
+      }).catch((er) => {
+        throw new Error(er);
+      });
+    }).then((next) => { // check
+      const targetCnt = fs.readFileSync(distPath).toString();
+
+      expect(targetCnt.match(REG.HTML_LINK_TAG_INLINE)).to.equal(null);
+      expect(targetCnt.match(REG.HTML_SCRIPT_TAG_INLINE)).to.equal(null);
+
+      targetCnt
+        // 检查所有路径都是正确的
+        .replace(REG.HTML_PATH_REG, (str, $1, $2, $3, $4) => {
+          const iPath = path.join(path.dirname(distPath), $4);
+          // console.log([iPath, fs.existsSync(iPath)]);
+          expect(fs.existsSync(iPath)).to.deep.equal(true);
+          return str;
+        })
+        // 检查 js 不会出现 __inline 方法
+        .replace(REG.HTML_SCRIPT_CONTENT, (str, $1, $2) => {
+          const iCnt = $2;
+          expect(iCnt.match(REG.JS_INLINE)).to.equal(null);
+          return str;
+        })
+        // 检查 style 中 路径都是正确的
+        .replace(REG.HTML_STYLE_CONTENT, (str, $1, $2) => {
+          const iCnt = $2;
+          const replaceHandle = (str, $1, $2) => {
+            const iPath = $2;
+            if (iPath.match(REG.CSS_IGNORE_REG)) {
+              return str;
+            }
+
+            if (iPath.match(REG.IS_HTTP) || iPath.match(REG.CSS_IS_ABSLURE)) {
+              return str;
+            }
+
+            const fullPath = path.join(path.dirname(distPath), iPath);
+            // console.log([fullPath, fs.existsSync(fullPath)]);
+            expect(fs.existsSync(fullPath)).to.equal(true);
+            return str;
+          };
+          iCnt
+            .replace(REG.CSS_PATH_REG, replaceHandle)
+            .replace(REG.CSS_PATH_REG2, replaceHandle);
+          return str;
+        });
+      next();
+    }).then((next) => { // clear frag
+      fn.frag.destroy().then(() => {
+        next();
+      });
+    }).then(() => { // finished
+      done();
+    }).start();
+  });
+
+  it('yyl-inlinesource({type: js}) abs test', function(done) {
+    this.timeout(0);
+    const srcPath = path.join(FRAG_PATH, '/js/demo.js');
+    const distPath = path.join(FRAG_PATH, 'r.js');
+    new util.Promise((next) => { // clear frag
+      fn.frag.destroy().then(() => {
+        fn.frag.build().then(() => {
+          next();
+        });
+      });
+    }).then((next) => { // copy files
+      const sourcePath = path.join(__dirname, 'dist');
+      util.copyFiles(sourcePath, FRAG_PATH, () => {
+        next();
+      });
+    }).then((next) => { // run tasks
+      inlinesource({
+        baseUrl: path.dirname(srcPath),
+        publishPath: '/js/',
+        type: 'js',
+        content: fs.readFileSync(srcPath)
+      }).then((cnt) => {
+        fs.writeFileSync(distPath, cnt);
+        next();
+      }).catch((er) => {
+        throw new Error(er);
+      });
+    }).then((next) => { // check
+      const targetCnt = fs.readFileSync(distPath).toString();
+      expect(targetCnt).not.equal('');
+      // 检查 js 不会出现 __inline 方法
+      expect(targetCnt.match(REG.JS_INLINE)).to.equal(null);
+      next();
+    }).then((next) => { // clear frag
+      fn.frag.destroy().then(() => {
+        next();
+      });
+    }).then(() => { // finished
+      done();
+    }).start();
+  });
+  it('yyl-inlinesource({type: css}) abs test', function(done) {
+    this.timeout(0);
+    const srcPath = path.join(FRAG_PATH, '/css/demo.css');
+    const distPath = path.join(FRAG_PATH, 'r.css');
+    new util.Promise((next) => { // clear frag
+      fn.frag.destroy().then(() => {
+        fn.frag.build().then(() => {
+          next();
+        });
+      });
+    }).then((next) => { // copy files
+      const sourcePath = path.join(__dirname, 'dist');
+      util.copyFiles(sourcePath, FRAG_PATH, () => {
+        next();
+      });
+    }).then((next) => { // run tasks
+      inlinesource({
+        baseUrl: path.dirname(srcPath),
+        publishPath: '/css/',
+        type: 'css',
+        content: fs.readFileSync(srcPath)
+      }).then((cnt) => {
+        fs.writeFileSync(distPath, cnt);
+        next();
+      }).catch((er) => {
+        throw new Error(er);
+      });
+    }).then((next) => { // check
+      const iCnt = fs.readFileSync(distPath).toString();
+      expect(iCnt).not.equal('');
+      const replaceHandle = (str, $1, $2) => {
+        const iPath = $2;
+        if (iPath.match(REG.CSS_IGNORE_REG)) {
+          return str;
+        }
+
+        if (iPath.match(REG.IS_HTTP) || iPath.match(REG.CSS_IS_ABSLURE)) {
+          return str;
+        }
+
+        const fullPath = path.join(path.dirname(distPath), iPath);
+        // console.log(fullPath, fs.existsSync(fullPath));
+        expect(fs.existsSync(fullPath)).to.equal(true);
+        return str;
+      };
+      iCnt
+        .replace(REG.CSS_PATH_REG, replaceHandle)
+        .replace(REG.CSS_PATH_REG2, replaceHandle);
+      next();
+    }).then((next) => { // clear frag
+      fn.frag.destroy().then(() => {
+        next();
+      });
+    }).then(() => { // finished
+      done();
+    }).start();
+  });
 });
